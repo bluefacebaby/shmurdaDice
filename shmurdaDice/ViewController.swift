17bd2c10ada7473e884aa03ff5258fcaef6469d6
Uwaifiokun Idugboe <uwaifiokuni1@109d25lx1rwf8j7.local>
2019-02-07 13:48:51 -0700

    Initial Commit

---
+++ shmurdaDice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shmurdaDice
+//
+//  Created by Uwaifiokun Idugboe on 2019-02-07.
+//  Copyright © 2019 Uwa Idugboe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
