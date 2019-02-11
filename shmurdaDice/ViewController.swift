215b97730d5da456f54d5ac3e35c26fcb1b9344e
Uwaifiokun Idugboe <uwaifiokuni1@109d25lx1rwf8j7.local>
2019-02-11 13:43:34 -0700

    linked design to code

--- shmurdaDice/ViewController.swift
+++ shmurdaDice/ViewController.swift
@@ -9,13 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
     @IBOutlet weak var diceimageview1: UIImageView!
-    
+    @IBOutlet weak var diceImageView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func rollButtonPress(_ sender: UIButton) {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+    }
 }
 
