77341e6db4c44b52a5d4da84dc5274b7661fbd0c
Uwaifiokun Idugboe <uwaifiokuni1@109d25lx1rwf8j7.local>
2019-02-19 13:30:51 -0700

    done

--- shmurdaDice/ViewController.swift
+++ shmurdaDice/ViewController.swift
@@ -11,17 +11,36 @@ import UIKit
 class ViewController: UIViewController {
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6" ]
     @IBOutlet weak var diceimageview1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDiceImage()
     }
 
 
     @IBAction func rollButtonPress(_ sender: UIButton) {
+        updateDiceImage()
+        
+    }
+
+    // This function generates 2 random numbers for the dice.
+    func updateDiceImage(){
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
+        print(randomDiceIndex1)
+        
+        diceimageview1.image = UIImage(named:diceArray[randomDiceIndex1] )
+        diceImageView2.image = UIImage(named:diceArray[randomDiceIndex2] )
+   
+    }
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImage()
     }
 }
+
 
